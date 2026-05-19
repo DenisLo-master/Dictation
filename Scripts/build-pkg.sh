c81082e295aa48026720f99d7f02827639c5f739
Denis <denis.lkg@gmail.com>
2026-05-20 00:50:14 +0600

    Cut app icon background

--- Scripts/build-pkg.sh
+++ Scripts/build-pkg.sh
@@ -71,7 +71,7 @@ COPYFILE_DISABLE=1 COPY_EXTENDED_ATTRIBUTES_DISABLE=1 pkgbuild \
   --scripts "$PKG_SCRIPTS" \
   --install-location "/" \
   --identifier "dev.denis.Dictation.pkg" \
-  --version "0.1.4" \
+  --version "0.1.5" \
   "$PKG_PATH"
 
 echo "Built $PKG_PATH"
